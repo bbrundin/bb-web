d846775366423d234391540c3e3cc163d150dd9a
Brittany Brundin <bbrundin112@gmail.com>
2017-05-27 23:30:30 -0400

    Modified styles.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">Britt Brundin</p>
-        <p>Hi world</p>
+        <div className="App-page">
+          <p className="App-intro">Britt Brundin</p>
+          <p>Hi world</p>
+          <ul>
+            <li>howdy</li>
+            <li>hoooo</li>
+          </ul>
+        </div>
       </div>
     );
   }
